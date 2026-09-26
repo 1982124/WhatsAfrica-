080db185b92a13793a71d16871a9f303d8f4924f
Neo Digital Startup Academy (NDSA) <promarketer1er@gmail.com>
2026-09-26 02:32:03 +0200

    redteam: remove superseded migration filename 20260926030000_redteam_consolidate_digital_entitlement_triggers.sql

--- supabase/migrations/20260926030000_redteam_consolidate_digital_entitlement_triggers.sql
+++
@@ -1,5 +0,0 @@
--- Consolidate paid-order digital entitlement issuance to one trigger.
-drop trigger if exists trg_orders_after_insert_digital_entitlements on public.orders;
-drop trigger if exists trg_orders_issue_digital_entitlements on public.orders;
-drop function if exists private.trg_orders_after_insert_digital_entitlements();
-drop function if exists private.trg_issue_digital_entitlements();
